35a20d5f7048282eabf7db164242a2e58c34fed9
venkatvenkat-svg <53077792+venkatvenkat-svg@users.noreply.github.com>
2019-07-19 15:01:39 +0530

    Create vowel_or_not.py

---
+++ vowel_or_not.py
@@ -0,0 +1,11 @@
+#checking vowel or not
+vowels=['a','e','i','o','u']
+consonants=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+ch=input("enter a alphabet")
+a=ch.lower()
+if a in vowels:
+    print("Vowel")
+elif(a in consonants):
+    print("Consnant")
+else:
+    print("invalid")
