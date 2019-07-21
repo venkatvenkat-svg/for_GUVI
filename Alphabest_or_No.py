f120b10d0f9631be2e0151dd0a32cdabbe1fcebc
venkatvenkat-svg <53077792+venkatvenkat-svg@users.noreply.github.com>
2019-07-21 14:41:39 +0530

    Create Alphabest_or_No.py

---
+++ Alphabest_or_No.py
@@ -0,0 +1,15 @@
+#check Alphabet or not
+string="abcdefghijklmnopqrstuvwxyz"
+alpha=list(string)#creating a list of alphabets
+
+ch=input() #assign the input to "ch"
+b=ch.lower()
+if ch in alpha:
+    a=ord(b)
+    print(a)
+    if a in range(a,a+25):
+        print("Alphbet")
+    else:
+        print("No")
+else:
+    print("No")
